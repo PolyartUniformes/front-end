bfb009ec1df37de298ce6a70b8cd218089fb63c5
Nextti <105963017+WalberthLopes@users.noreply.github.com>
2022-12-22 23:25:28 -0300

    final commit

--- src/modules/mostruario/hist/hist-items.tsx
+++ src/modules/mostruario/hist/hist-items.tsx
@@ -15,16 +15,19 @@ const Items = ({ items }: any) => {
         <div
           style={{
             display: "grid",
-            gridTemplateColumns: "50px auto 300px 300px 120px 120px 25px",
+            gridTemplateColumns:
+              "25px 120px 130px auto 150px 230px 80px 80px 25px",
             textAlign: "center",
           }}
         >
           <p>ID</p>
           <p>CLIENTE</p>
+          <p>TELEFONE</p>
+          <p>ENDEREÇO</p>
           <p>LOTE</p>
           <p>ITEM</p>
-          <p>DATA ALUGUEL</p>
-          <p>DATA ENTREGA</p>
+          <p>ALUGUEL</p>
+          <p>ENTREGA</p>
           <p></p>
         </div>
         {items &&
@@ -34,48 +37,64 @@ const Items = ({ items }: any) => {
                 key={index}
                 style={{
                   display: "grid",
-                  gridTemplateColumns: "50px auto 300px 300px 120px 120px 25px",
+                  gridTemplateColumns:
+                    "25px 120px 130px auto 150px 230px 80px 80px 25px",
+                  fontSize: "10px",
                 }}
               >
                 <input
                   type="text"
-                  name="id"
-                  style={{ textAlign: "center" }}
+                  name="tamanho"
+                  style={{ textAlign: "center", fontSize: "10px" }}
                   value={element.id}
                   readOnly
                 />
                 <input
                   type="text"
                   name="tamanho"
-                  style={{ textAlign: "center" }}
+                  style={{ textAlign: "center", fontSize: "10px" }}
                   value={element.cliente}
                   readOnly
                 />
                 <input
                   type="text"
+                  name="id"
+                  style={{ textAlign: "center", fontSize: "10px" }}
+                  value={element.telefone}
+                  readOnly
+                />
+                <input
+                  type="text"
                   name="tamanho"
-                  style={{ textAlign: "center" }}
+                  style={{ textAlign: "center", fontSize: "10px" }}
+                  value={element.endereco}
+                  readOnly
+                />
+                <input
+                  type="text"
+                  name="tamanho"
+                  style={{ textAlign: "center", fontSize: "10px" }}
                   value={element.lote}
                   readOnly
                 />
                 <input
                   type="text"
                   name="tamanho"
-                  style={{ textAlign: "center" }}
+                  style={{ textAlign: "center", fontSize: "10px" }}
                   value={element.item}
                   readOnly
                 />
                 <input
                   type="text"
                   name="item"
-                  style={{ textAlign: "center" }}
+                  style={{ textAlign: "center", fontSize: "10px" }}
                   value={dateChange(element.dia_alugado)}
                   readOnly
                 />
                 <input
                   type="text"
                   name="item"
-                  style={{ textAlign: "center" }}
+                  style={{ textAlign: "center", fontSize: "10px" }}
                   value={
                     element.dia_entregue ? element.dia_entregue : "NÃO ENTREGUE"
                   }
